6aec80cc0975f028a4bc23c10c9fb6d5fa3338a3
Иванов Сергей <s.ivanov@mkk-finlite.lcl>
2022-09-09 15:28:10 +0300

    Add homework2

--- lesson2/les2-dz2.py
+++ lesson2/les2-dz2.py
@@ -3,3 +3,9 @@
 # Пример:
 
 # - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+n = int(input('input N: '))
+factorial = 1
+for i in range(1, n+1):
+    factorial *= i
+    print(factorial, end=' ')
