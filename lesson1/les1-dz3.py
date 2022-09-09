b319eb0a59c5937ba8e1528a9467589e8cfa9b48
Иванов Сергей <s.ivanov@mkk-finlite.lcl>
2022-09-09 12:09:24 +0300

    Modified les1-dz3.py

--- lesson1/les1-dz3.py
+++ lesson1/les1-dz3.py
@@ -1,32 +1,49 @@
 # Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
 # в которой находится эта точка (или на какой оси она находится).
+
 # Пример:
+
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
-def inputKoord(x):
-    a = [0] * x
-    for i in range(x):
-        is_OK = False
-        while not is_OK:
-            try:
-                number = float(input(f"Введите {i+1} координату: "))
-                a[i] = number
-                is_OK = True
-                if a[i] == 0:
-                    is_OK = False
-                    print("Координата не должно быть равна 0 ")
-            except ValueError:
-                print("Ты ошибся. Вводить надо числа!")
-    return a
-def checkCoordinates(xy):
-    text = 4
-    if xy[0] > 0 and xy[1] > 0:
-        text = 1
-    elif xy[0] < 0 and xy[1] > 0:
-        text = 2
-    elif xy[0] < 0 and xy[1] < 0:
-        text = 3
-    print(f"Точка находится в {text} четверти плоскости")
-koordinate = inputKoord(2)
-checkCoordinates(koordinate)
+
+x = int(input('Введите координату точки X: '))
+y = int(input('Введите координату точки Y: '))
+
+if x == 0 or y == 0:
+    print('x=',x,'; y=',y,' -> условие X ≠ 0 и Y ≠ 0 не соблюдено')
+elif x > 0 and y > 0:
+    print('x=',x,'; y=',y,' -> 1')
+elif x < 0 and y > 0:
+    print('x=',x,'; y=',y,' -> 2')
+elif x < 0 and y < 0:
+    print('x=',x,'; y=',y,' -> 3')
+elif x > 0 and y < 0:
+    print('x=',x,'; y=',y,' -> 4')
+
+# def inputKoord(x):
+#     a = [0] * x
+#     for i in range(x):
+#         is_OK = False
+#         while not is_OK:
+#             try:
+#                 number = float(input(f"Введите {i+1} координату: "))
+#                 a[i] = number
+#                 is_OK = True
+#                 if a[i] == 0:
+#                     is_OK = False
+#                     print("Координата не должно быть равна 0 ")
+#             except ValueError:
+#                 print("Ты ошибся. Вводить надо числа!")
+#     return a
+# def checkCoordinates(xy):
+#     text = 4
+#     if xy[0] > 0 and xy[1] > 0:
+#         text = 1
+#     elif xy[0] < 0 and xy[1] > 0:
+#         text = 2
+#     elif xy[0] < 0 and xy[1] < 0:
+#         text = 3
+#     print(f"Точка находится в {text} четверти плоскости")
+# koordinate = inputKoord(2)
+# checkCoordinates(koordinate)
