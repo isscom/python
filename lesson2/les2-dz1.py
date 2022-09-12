948732904554858ed6848ed684a2bf3a1c79c8a5
Иванов Сергей <s.ivanov@mkk-finlite.lcl>
2022-09-12 12:46:07 +0300

    Add lesson3 dz

--- lesson2/les2-dz1.py
+++ lesson2/les2-dz1.py
@@ -6,9 +6,8 @@
 # - 0,56 -> 11
 
 float_num = input('input float number: ')
-print(type(float_num))
 sum = 0
 for i in float_num:
     if i != '.':
         sum += int(i)
-print(sum)
+print(f'{float_num} -> {sum}')
