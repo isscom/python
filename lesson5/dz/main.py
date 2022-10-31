d41b8871e536a29d1c661de0180a336ecca447bd
Иванов Сергей <s.ivanov@mkk-finlite.lcl>
2022-10-31 15:20:34 +0300

    add all tsks lesson 5

--- lesson5/dz/main.py
+++ lesson5/dz/main.py
@@ -31,6 +31,27 @@ print(result_list)
 # *Пример:*
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
+import math
+list1 = [1.1, 1.2, 3.1, 5, 10.01]
+list2 = []
+for i in range(len(list1)):
+    a = round(list1[i]*10 % 10, 2)
+    if a > 0:
+        list2.append(a)
+b = round(float((max(list2)-min(list2))*10/100), 2)
+print(b) # 0.19
+
+from unittest import result
+my_list = [1.1, 1.2, 3.1, 5, 10.01]
+min = 1
+max = 0
+for i in my_list:
+    if (i - int(i)) <= min:
+        min = i - int(i)
+    if (i - int(i)) >= max:
+        max = i - int(i)
+print('%.2f' % (max-min)) # 0.20
+
 
 # 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 # *Пример:*
@@ -38,7 +59,35 @@ print(result_list)
 # - 3 -> 11
 # - 2 -> 10
 
+N = int(input('Введите число в десятичной СИ: '))
+D = ''
+while N > 0:
+    D = str(N%2) + D
+    N = N // 2
+print(D)
+
+
+a=int(input('Введите число: '))
+b=bin(a)
+print(b[2:])
+
 
 # 5. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 # *Пример:*
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+a = int(input('Enter the number: '))
+print(a)
+negofibonacci = [1,-1]
+fibonacci = [1,1]
+
+for i in range(2,a):
+    list = fibonacci[i-1]+fibonacci[i-2]
+    fibonacci.append(list)
+    list_nego = negofibonacci[i-2] - negofibonacci[i-1]
+    negofibonacci.append(list_nego)
+
+negofibonacci.reverse()
+negofibonacci.append(0)
+
+print(f' for a = {a} =>{negofibonacci+fibonacci}')
