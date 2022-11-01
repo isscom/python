653c11d9278b793aa8b2908f617687ce8d1a9773
Иванов Сергей <s.ivanov@mkk-finlite.lcl>
2022-11-01 10:29:08 +0300

    Done hometask5 + done 1 task

---
+++ lesson6/razbor_dz/main.py
@@ -0,0 +1,104 @@
+# 1. Вычислить число c заданной точностью *d*
+# *Пример:*
+# - при $d = 0.001, π = 3.141.$ $10^{-1} ≤ d ≤10^{-10}$
+
+from math import pi
+
+d =  int(input("Введите число для заданной точности числа Пи:\n"))
+print(f'число Пи с заданной точностью {d} равно {round(pi, d)}')
+
+
+
+
+# 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+def factors(num, d=2):
+    while num > 1:
+        n1, n2 = divmod(num, d)
+        if n2:
+            d += 1
+        else:
+            yield d
+            num = n1
+
+n = int(input("Задайте натуральное число N: "))
+print('{} = {}' .format(n, ' * '.join(map(str, factors(n)))))
+
+
+
+# 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+lst = list(map(int, input("Введите числа через пробел:\n").split()))
+print(f"Исходный список: {lst}")
+new_lst = []
+[new_lst.append(i) for i in lst if i not in new_lst]
+print(f"Список из неповторяющихся элементов: {new_lst}")
+
+
+
+# 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# *Пример:*
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+import random
+
+def write_file(st):
+    with open('file33.txt', 'w') as data:
+        data.write(st)
+
+def rnd():
+    return random.randint(0,101)
+
+def create_mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+    
+def create_str(sp):
+    lst= sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
+k = int(input("Введите натуральную степень k = "))
+koef = create_mn(k)
+write_file(create_str(koef))
+
+
+
+
+# 5. Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
+
+with open('poly_1.txt', 'w', encoding='utf-8') as file:
+    file.write('2*x^2 + 5*x^5')
+with open('poly_2.txt', 'w', encoding='utf-8') as file:
+    file.write('23*x^4 + 9*x^6')
+
+with open('poly_1.txt','r') as file:
+    poly_1 = file.readline()
+    list_of_poly_1 = poly_1.split()
+
+
+with open('poly_2.txt','r') as file:
+    poly_2 = file.readline()
+    list_of_poly_2 = poly_2.split()
+
+print(f'{list_of_poly_1} + {list_of_poly_2}')
+sum_poly = list_of_poly_1 + list_of_poly_2
+
+with open('sum_poly.txt', 'w', encoding='utf-8') as file:
+    file.write(f'{list_of_poly_1} + {list_of_poly_2}')
