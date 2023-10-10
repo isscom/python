7bc7e63bcc31edc40739fffd6ad3eff1fff937c6
Иванов Сергей <s.ivanov@mkk-finlite.lcl>
2023-10-10 14:25:09 +0300

    Add lesson4, homework3 and homework3 (create)

--- python2.0/lesson3/task18.py
+++ python2.0/lesson3/task18.py
@@ -2,5 +2,14 @@
 # величине элемент к заданному числу X. Пользователь в первой строке
 # вводит натуральное число N – количество элементов в массиве. В
 # последующих строках записаны N целых чисел Ai
-# . Последняя строка
-# содержит число X
+# Последняя строка содержит число X
+
+n = int(input('N: '))
+a = [i for i in range(1, n+1)]
+element_result = a[0]
+print(a)
+x = int(input('Х: '))
+for element in a:
+    if abs(element - x) < abs(element_result - x):
+        element_result = element
+print(f'-> {element_result}')
