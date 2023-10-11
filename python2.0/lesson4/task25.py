e7f8dfa680c75f78fc485dee5586f5557586e16b
Иванов Сергей <s.ivanov@mkk-finlite.lcl>
2023-10-11 10:00:03 +0300

    fix 25 task

--- python2.0/lesson4/task25.py
+++ python2.0/lesson4/task25.py
@@ -9,3 +9,41 @@
 
 # Для решения данной задачи используйте функцию
 # .split()
+
+# РЕШЕНИЕ 1
+# input_string = [i for i in input("Input: ").split()] # генератором списков задаем список (строку)
+# output_string = set(input_string)
+# for i in output_string:
+#     count = 0
+#     for j in range(len(input_string)):
+#         if i == input_string[j]:
+#             if count > 0: input_string[j] = f'{input_string[j]}_{count}'
+#             count += 1
+# print('Output:', end = " ")
+# print(*input_string)
+##################################################
+
+#РЕШЕНИЕ 2 (со словарём)
+input_string = [i for i in input("Input: ").split()] # вводим строку и создаём из нее список
+print('Output:', end = " ") # end = " " это в конце принта - не переход на новую строку, а просто пробел (то что в кавычках)
+letters = {} # создаем словарь
+for i in input_string: # проходим по списку
+    if i in letters.keys(): # сравниваем каждый элемент с ключами
+        print(i + "_" + str(letters[i]), end = " ") # если ключ есть - добавляем ключ + нижнее подчеркивание + номер (сколько раз оно встретилось на текущий момент)
+        letters[i] += 1 # увеличиваем кол-во раз на единицу
+    else:
+        print(i, end = " ") # если ключа не нашлось, то просто выводим букву ниже
+        letters[i] = 1
+##################################################
+
+# # РЕШЕНИЕ 3
+# input_string = input("Input: ").split(' ') # ввод строки (input) и разбиваем строку в список (split) по разделителю пробел (' ')
+# output_string = ''.strip() # задаём строку, которая будет результирующей, а strip() это что?
+# for i in input_string:
+#     c = output_string.count(i)
+#     if output_string.count(i) == 0:
+#         output_string += i + ' '
+#     else:
+#         output_string += i + '_' + str(c) + ' '
+# print(f'Output: {output_string}')
+##################################################
