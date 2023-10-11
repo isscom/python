fed1d4d831fabae2db4ad0a7d0d31607ab9f82bb
Иванов Сергей <s.ivanov@mkk-finlite.lcl>
2023-10-11 10:40:28 +0300

    Add task27

--- python2.0/lesson4/task27.py
+++ python2.0/lesson4/task27.py
@@ -5,9 +5,21 @@
 # пробелов или символами конца строки.Определите,
 # сколько различных слов содержится в этом тексте.
 
-# Input: She sells sea shells on the sea shore;The shells
-# that she sells are sea shells I'm sure.So if she sells sea
-# shells on the sea shore,I'm sure that the shells are sea
-# shore shells.
+# Input: She sells sea shells on the sea shore; The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore, I'm sure that the shells are sea shore shells.
+# Output: 17
 
-# Output: 19
+text = "She sells sea shells on the sea shore; The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore, I'm sure that the shells are sea shore shells."
+print(f'Input: {text}')
+# print(f'Output: {len(set(text.upper().split()))}')
+
+# РЕШЕНИЕ НА ОТДЕЛЬНЫХ СТРОКАХ:
+a = text.upper() # всё большими буквами
+# SHE SELLS SEA SHELLS ON THE SEA SHORE; THE SHELLS THAT SHE SELLS ARE SEA SHELLS I'M SURE. SO IF SHE SELLS SEA SHELLS ON THE SEA SHORE, I'M SURE THAT THE SHELLS ARE SEA SHORE SHELLS.
+b = a.split() # делаем список из строки
+# ['SHE', 'SELLS', 'SEA', 'SHELLS', 'ON', 'THE', 'SEA', 'SHORE;', 'THE', 'SHELLS', 'THAT', 'SHE', 'SELLS', 'ARE', 'SEA', 'SHELLS', "I'M", 'SURE.', 'SO', 'IF', 'SHE', 'SELLS', 'SEA', 'SHELLS', 'ON', 'THE', 'SEA', 'SHORE,', "I'M", 'SURE', 'THAT', 'THE', 'SHELLS', 'ARE', 'SEA', 'SHORE', 'SHELLS.']
+c = set(b) # превращаем список в множество (в с - те слова, кот. не повторяются)
+# {'SEA', 'SURE', 'THAT', 'SHE', 'SELLS', 'THE', 'SHELLS.', 'SHORE;', 'SHELLS', 'ARE', 'IF', 'SHORE,', "I'M", 'SURE.', 'SHORE', 'ON', 'SO'}
+d = len(c) # определяем кол-во элементов множества (явл. решением задачи)
+# 17
+# И запишем всё в одну строку:
+print(f'Output: {len(set(text.upper().split()))}')
