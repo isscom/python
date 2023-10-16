c996f6d1be72c1073ea8ab0af0ad01fbe5bca879
Иванов Сергей <s.ivanov@mkk-finlite.lcl>
2023-10-16 10:40:42 +0300

    Add file seminar7.py + comment ht32

--- python2.0/lesson6/task32 (hm).py
+++ python2.0/lesson6/task32 (hm).py
@@ -5,6 +5,7 @@
 # Вывод: [1, 9, 13, 14, 19]
 
 list_input = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+#              0  1  2  3  4   5   6  7   8  9   10 11  12 13 14  15  16 17  18  19
 
 # min_number = int(input('min_number: '))
 # max_number = int(input('max_number: '))
