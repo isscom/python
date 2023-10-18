7d75b581f8c5d185eddd343130307de0d2937b65
Иванов Сергей <s.ivanov@mkk-finlite.lcl>
2023-10-18 09:54:21 +0300

    Mod telephone_dir

--- python2.0/lesson8/telephone_directory/commands.py
+++ python2.0/lesson8/telephone_directory/commands.py
@@ -1,11 +1,14 @@
 def show_data():
     with open('sprav.txt', 'r', encoding='utf-8') as sprav:
+        print('Текущие записи:')
         print(sprav.readlines())
 
 def save_data_in_txt():
     with open('sprav.txt', 'a', encoding='utf-8') as sprav:
         sprav.write(input('Введите запись в формате Имя;+77777777;Город\n'))
+        print('Запись успешно сохранена!')
         sprav.write('\n')
 
 def find_data():
-    print('Поиск определенной записи по характеристике')
+    what_find = str(input('Введите поисковый запрос: '))
+    
